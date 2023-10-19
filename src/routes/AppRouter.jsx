7ea5ef0a9de3bc879444cea3dcfc854968c30429
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-10-19 18:14:44 -0500

    🌟ADD: 👷🏽‍♂️ Add Routes

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Link, Route, Routes, Navigate } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar";
+
+const AuthStatus = {
+  notAuthenticated: "not-authenticated",
+  authenticated: "authenticated",
+}
+
+export const AppRouter = () => {
+  const authStatus = AuthStatus.notAuthenticated;
+  console.log(authStatus)
+  return (
+    <Routes>
+      {authStatus === AuthStatus.notAuthenticated ? (
+        <Route path='/auth/*' element={<LoginPage />} />
+      ) : (
+        <Route path='/*' element={<CalendarPage />} />
+      )}
+
+      <Route path='/*' element={<Navigate to="/auth/login" />} />
+    </Routes>
+  );
+};
